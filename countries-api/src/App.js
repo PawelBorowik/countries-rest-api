87de6c78c14a4b8dd56a9c91ec7adeb384b98ba0
Pawel Borowik <pawelbor1@op.pl>
2020-04-03 17:47:32 +0200

    added useReducer to App

--- countries-api/src/App.js
+++ countries-api/src/App.js
@@ -1,43 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import "./App.css";
 
-function App() {
-  const [countries, setCountries] = useState([]);
-  const [selectItem, setSelectItem] = useState("");
-  const [loading, setLoading]= useState(true)
-  const [error, setError]= useState()
+function dataUseReducer(state, action){
+  switch(action.type){
+    case "FETCH_START":
+      return {
+        ...state, 
+      }
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          countries:action.countries, 
+          loading:false}
+      case "FETCH_FAILED":
+        return {
+          ...state, 
+          countries:[], 
+          loading:false,
+          error: action.error}
+      default:
+            return null   
+  }
+}
 
-  const API = "https://restcountries.eu/rest/v2/";
+function App() {
+  // const [countries, setCountries] = useState(null);
+  const [selectCountry, setSelectCountry] = useState("");
+  // const [loading, setLoading]= useState(true)
+  // const [error, setError]= useState(null)
+ const [state, dispatch] =useReducer(
+dataUseReducer,
+{
+  countries:[],
+  loading:true,
+  error:null,
+}
+)
+
+const API = "https://restcountries.eu/rest/v2/";
 
   useEffect(()=>{
     const myFetch= async()=>{
-    
+    dispatch({type:"FETCH_START"})
       try{
          const response =await fetch(API)
          const responseParsed= await response.json()
-         setLoading(false)
-         setCountries(responseParsed)
+         dispatch({type:"FETCH_SUCCESS", countries: responseParsed})
+        //  setLoading(false)
+        //  setCountries(responseParsed)
          }catch(error){
-          setLoading(false)
-          setError(error)
+          dispatch({type:"FETCH_FAILED", error: error})
+          // setLoading(false)
+          // setError(error)
          }
         }
         myFetch()
 
-  },  [selectItem])
+  }, [selectCountry])
 
-  const handleChangeSelect = e => setSelectItem(e.target.value);
+  const handleChangeSelect = e => setSelectCountry(e.target.value);
   
-  const showCountries = countries.map(country => (
+  const showCountries = state.countries.map(country => (
     <option key={country.name} value={country.name}>
       {country.name}
     </option>
   ));
 
-  const selectedCountry = countries.filter(
-    country => country.name === selectItem
+  const selectedCountry = state.countries.filter(
+    country => country.name === selectCountry
   );
-  const flag = selectedCountry.map(selectCountry => (
+  const showSelectedCountry = selectedCountry.map(selectCountry => (
     <div key={selectCountry.name}>
      <img src={selectCountry.flag} alt={selectCountry.alpha3Code}></img>
       <p>{selectCountry.name}</p>
@@ -50,15 +82,14 @@ const select= <select onChange={handleChangeSelect} >
 {showCountries}
 </select>
   
-if(error){
+if(state.error){
   return <p>error</p>
 }
-
   return (
     <div className="App">
       <header className="App-header">
-        <div className="hhh">{loading ? <p>loading...</p>: select}</div>
-        <div className="flag"> {flag}</div>
+        <div className="hhh">{state.loading ? <p>loading...</p>: select}</div>
+        <div className="flag"> {showSelectedCountry}</div>
       </header>
     </div>
   );
